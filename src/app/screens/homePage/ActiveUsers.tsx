5c3daf32d3aaf8badc99c6ffd7d6949cda362195
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2024-07-14 10:14:16 +0900

    fix: homepage related sectional components

---
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export default function ActiveUsers() {
+    return <div>ActiveUsers</div>
+}
---
+++ src/app/screens/homePage/NewDishes.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NewDishes() {
+    return <div>NewDishes</div>
+}
